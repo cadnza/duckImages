0b3afd8d45f94ec7227e1bd21e405974f35299ef
Jon Dayley <jonathandayley@gmail.com>
2025-07-07 20:40:56 -0600

    Format

--- tests/testthat/test-searchWorks.R
+++ tests/testthat/test-searchWorks.R
@@ -1,13 +1,13 @@
 testthat::test_that("search works", {
-	final <- imageSearch(
-		"oranges",
-		page=1,
-		type="photo",
-		lay=NA
-	)
-	testthat::expect_true(
-		class(final[1])=="character",
-		info="An NA result means that curl_download failed.",
-		label="Curl result"
-	)
+    final <- imageSearch(
+        "oranges",
+        page = 1,
+        type = "photo",
+        lay = NA
+    )
+    testthat::expect_true(
+        class(final[1]) == "character",
+        info = "An NA result means that curl_download failed.",
+        label = "Curl result"
+    )
 })
